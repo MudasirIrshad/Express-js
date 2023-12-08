c4a2dc53ff2135dd58f0ce523440a7dcf79ed6d4
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-12-08 23:22:32 +0500

    Applicable for Vote or not using middleware

---
+++ BodyParser_Middleware/index.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const app=express()
+const port=3000
+const bodyParser=require('body-parser')
+
+app.use(bodyParser.json())
+
+function VoteMiddleWare(req,res,next){
+    let age=req.body.age
+    if(age>=18) next()
+    else {
+        let obj={
+            age,
+            err:"Age is less than 18"
+        }
+        res.send(obj)
+    }
+}
+app.use(VoteMiddleWare)
+
+
+app.post('/Vote',(req,res)=>{res.send(`Applicable for Vote, Your age: ${req.body.age}`)})
+
+
+
+app.listen(port)
