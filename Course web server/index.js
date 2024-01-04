ca507ff158e18181f7824b47e4dc5ecb15f9d0d8
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-01-04 23:45:21 +0500

    Admin Login

--- Course web server/index.js
+++ Course web server/index.js
@@ -10,18 +10,44 @@ let course=[]
 function adminMiddleWare(req,res,next){
     const {name,password}=req.body
     let admin=Admin.find(a => a.name=name && a.password==password)
+    console.log(admin);
+    if(Admin.length==0){
+        res.status(404).send('No Admin signed in');
+    }
+    else if(admin){
+        next()
+    }
+}
+
+app.post('/signup',(req,res,next)=>{
+    const {name,password}=req.body
+    let admin=Admin.find(a => a.name=name && a.password==password)
+    console.log(admin);
     if(admin){
-        res.status(404).send('Admin Already Exit');
+        if(Admin.length!=0){
+            res.status(404).send('Admin Exits');
+        }
     }
     else{
         Admin.push({name,password})
         next()
     }
-}
-
-app.post('/singup',adminMiddleWare,(req,res)=>{
-    res.send('Singup successfully'+Admin[0])
+},(req,res)=>{
+    res.send('Singup successfully')
 })
+app.post('/login',adminMiddleWare,(req,res)=>{
+    res.send('Loged in')
+})
+
+
+
+
+
+
+
+
+
+
 app.listen(port)
 
 
