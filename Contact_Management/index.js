0268d5170276bbb71eff7b0075cba19659a47d39
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-04-01 07:00:08 +0500

    User database

--- Contact_Management/index.js
+++ Contact_Management/index.js
@@ -1,11 +1,42 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const port = 3000
-const jwt = require('jsonwebtoken')
-const bodyParser=require('body-parser')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = 3000;
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const MongoURL =
+  "mongodb+srv://mudasirirshad47:mudasir123456789@cluster0.jzcnrjw.mongodb.net/Contact_Management";
 
+const userSignupSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+const Contacts = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
 
-app.listen(port, ()=>{
-    console.log('listening on port',port);
-})
+const MySelf = mongoose.model("MySelf", userSignupSchema);
+const MyContact = mongoose.model("MyContact", Contacts);
+mongoose.connect(MongoURL);
+
+app.post("/signup", async(req, res) => {
+  const name = req.body.name;
+  const number = req.body.number;
+  const user = await MySelf.findOne({number})
+  if(user){
+    res.send("User exits")
+  }
+  else{
+    const newUser = new MySelf({
+      name, number
+    });
+    newUser.save()
+    res.send("User saved")
+  }
+
+});
+app.listen(port, () => {
+  console.log("listening on port", port);
+});
